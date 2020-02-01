e82ecd1e48c119657a42ae856f97f53100744ec2
RiFlowTH <riflowth@gmail.com>
2020-02-01 21:52:13 +0700

    Refactored SpawnedNode statements

--- src/main/java/net/projectx/menecia/mob/MobSpawner.java
+++ src/main/java/net/projectx/menecia/mob/MobSpawner.java
@@ -7,8 +7,10 @@ import net.projectx.menecia.resources.utilities.Log;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityCombustEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
@@ -16,7 +18,6 @@ import org.bukkit.scheduler.BukkitTask;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 public class MobSpawner implements Listener {
 
@@ -25,8 +26,7 @@ public class MobSpawner implements Listener {
     private BukkitTask spawnerTask;
     private Map<Integer, Mob> mobMap = new HashMap<>();
     private int maxiumNodeSpawn = 5;
-    private Map<Integer, Integer> spawnedNodeMap = new HashMap<>();
-    private Map<UUID, Integer> spawnedMap = new HashMap<>();
+    private Map<Integer, Integer> spawnedCountMap = new HashMap<>();
 
     public MobSpawner(Core plugin) {
         this.plugin = plugin;
@@ -41,18 +41,15 @@ public class MobSpawner implements Listener {
     public void start() {
         Location spawnLocation = Bukkit.getWorlds().get(0).getSpawnLocation();
         spawnerTask = plugin.runTaskTimer(() -> {
-            if (!spawnedNodeMap.containsKey(StarvingZombie.ID)) {
-                spawnedNodeMap.put(StarvingZombie.ID, 0);
+            if (!spawnedCountMap.containsKey(StarvingZombie.ID)) {
+                spawnedCountMap.put(StarvingZombie.ID, 0);
             }
-            if (spawnedNodeMap.get(StarvingZombie.ID) < maxiumNodeSpawn) {
+            if (spawnedCountMap.get(StarvingZombie.ID) < maxiumNodeSpawn) {
                 Mob mob = mobMap.get(StarvingZombie.ID);
-                Entity entity = mobCreator.spawn(mob, spawnLocation);
-                int previousEntityInNode = spawnedNodeMap.get(StarvingZombie.ID);
-                spawnedNodeMap.put(StarvingZombie.ID, previousEntityInNode + 1);
-                int currentEntityInNode = spawnedNodeMap.get(StarvingZombie.ID);
-                spawnedMap.put(entity.getUniqueId(), StarvingZombie.ID);
+                mobCreator.spawn(mob, spawnLocation);
+                spawnedCountMap.put(StarvingZombie.ID, spawnedCountMap.get(StarvingZombie.ID) + 1);
                 Log.sendSuccess("Spawned 1 " + mob.getName() + " [Lv." + mob.getLevel() + "] "
-                        + "(" + currentEntityInNode + "/" + maxiumNodeSpawn + ")");
+                        + "(" + spawnedCountMap.get(StarvingZombie.ID) + "/" + maxiumNodeSpawn + ")");
             }
         }, 0, 20 * 20L);
     }
@@ -60,35 +57,37 @@ public class MobSpawner implements Listener {
     @EventHandler
     private void entityDeathEvent(EntityDeathEvent event) {
         Entity entity = event.getEntity();
-        PersistentDataContainer dataContainer = entity.getPersistentDataContainer();
-        boolean isMeneciaMob = dataContainer.has(plugin.getNamespacedKey(Key.MOB_ID), PersistentDataType.INTEGER);
-        if (isMeneciaMob) {
+        if (isMob(entity)) {
             Mob mob = mobMap.get(getMobId(entity));
-            UUID mobUuid = entity.getUniqueId();
-            int nodeId = spawnedMap.get(mobUuid);
-            int previousEntityInNode = spawnedNodeMap.get(nodeId);
-            spawnedNodeMap.put(nodeId, previousEntityInNode - 1);
-            int currentEntityInNode = spawnedNodeMap.get(nodeId);
-            spawnedMap.remove(mobUuid);
+            spawnedCountMap.put(mob.getId(), spawnedCountMap.get(mob.getId()) - 1);
             Log.sendWarning("Despawned 1 " + mob.getName() + " [Lv." + mob.getLevel() + "] "
-                    + "(" + currentEntityInNode + "/" + maxiumNodeSpawn + ")");
+                    + "(" + spawnedCountMap.get(mob.getId()) + "/" + maxiumNodeSpawn + ")");
+        }
+    }
+
+    @EventHandler
+    private void resetVanillaCombust(EntityCombustEvent event) {
+        if (event.getEntityType() == EntityType.ZOMBIE) {
+            event.setCancelled(true);
         }
     }
 
     public void clearAllMobs() {
         for (Entity entity : Bukkit.getWorlds().get(0).getEntities()) {
-            PersistentDataContainer dataContainer = entity.getPersistentDataContainer();
-            boolean isMob = dataContainer.has(plugin.getNamespacedKey(Key.MOB_ID), PersistentDataType.INTEGER);
-            if (isMob) {
+            if (isMob(entity)) {
                 entity.remove();
             }
         }
     }
 
-    private int getMobId(Entity entity) {
+    private boolean isMob(Entity entity) {
         PersistentDataContainer dataContainer = entity.getPersistentDataContainer();
-        boolean isMob = dataContainer.has(plugin.getNamespacedKey(Key.MOB_ID), PersistentDataType.INTEGER);
-        if (isMob) {
+        return dataContainer.has(plugin.getNamespacedKey(Key.MOB_ID), PersistentDataType.INTEGER);
+    }
+
+    private int getMobId(Entity entity) {
+        if (isMob(entity)) {
+            PersistentDataContainer dataContainer = entity.getPersistentDataContainer();
             return dataContainer.get(plugin.getNamespacedKey(Key.MOB_ID), PersistentDataType.INTEGER);
         }
         return 0;
