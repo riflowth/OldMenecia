f0e0dbf3ed75644c262d4124e024ddfd1ad7dd2d
RiFlowTH <riflowth@gmail.com>
2020-02-05 23:15:23 +0700

    Moved wrong statement to other class

--- src/main/java/net/projectx/menecia/mobs/events/MobDamageByBraveEvent.java
+++ src/main/java/net/projectx/menecia/mobs/events/MobDamageByBraveEvent.java
@@ -1,59 +1,47 @@
 package net.projectx.menecia.mobs.events;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.ListenerPriority;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.events.PacketEvent;
 import net.projectx.menecia.Core;
 import net.projectx.menecia.mobs.Mob;
 import net.projectx.menecia.mobs.Mobs;
+import net.projectx.menecia.resources.Icons;
+import net.projectx.menecia.resources.utilities.Hologram;
+import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class MobDamageByBraveEvent implements Listener {
 
     private Core plugin;
-    private ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
+    private static final int damageIndicatorShowTick = 10;
 
     public MobDamageByBraveEvent(Core plugin) {
         this.plugin = plugin;
-        removeDamageIndicatorParticle(plugin);
     }
 
     @EventHandler
     private void onEvent(EntityDamageByEntityEvent event) {
+        Entity damager = event.getDamager();
         Entity victim = event.getEntity();
-        if (Mobs.isMob(victim)) {
+        if ((damager instanceof Player) && (Mobs.isMob(victim))) {
             int mobId = Mobs.getId(victim);
             Mob mob = Mobs.get(mobId);
-
-            // TODO: Monster Health Bubble!
+            int damage = (int) event.getDamage();
+            Location victimLocation = victim.getLocation();
+            displayDamageHologram(damage, victimLocation);
         }
     }
 
-    private void removeDamageIndicatorParticle(Core plugin) {
-        protocolManager.addPacketListener(new PacketAdapter(plugin, ListenerPriority.NORMAL,
-                PacketType.Play.Server.WORLD_PARTICLES) {
-            @Override
-            public void onPacketSending(PacketEvent event) {
-                PacketContainer packet = event.getPacket();
-                if (event.getPacketType() == PacketType.Play.Server.WORLD_PARTICLES) {
-                    if (packet.getEntityModifier(event).read(0) instanceof Player) {
-                        int particleId = packet.getIntegers().read(0);
-                        int damageIndicatorParticleId = 7;
-                        if (particleId == damageIndicatorParticleId) {
-                            event.setCancelled(true);
-                        }
-                    }
-                }
-            }
-        });
+    private void displayDamageHologram(int damage, Location location) {
+        double x = ThreadLocalRandom.current().nextDouble(-1.25, 1.25);
+        double y = ThreadLocalRandom.current().nextDouble(0, 1.5);
+        double z = ThreadLocalRandom.current().nextDouble(-1.25, 1.25);
+        Hologram.drawTemporary("&c-" + damage + " &4" + Icons.RED_HEART, damageIndicatorShowTick,
+                location.toCenterLocation().add(x, y, z));
     }
 
 }
--- src/main/java/net/projectx/menecia/player/events/LevelingEvent.java
+++ src/main/java/net/projectx/menecia/player/events/LevelingEvent.java
@@ -1,18 +1,11 @@
 package net.projectx.menecia.player.events;
 
 import net.projectx.menecia.Core;
-import net.projectx.menecia.resources.Icons;
-import net.projectx.menecia.resources.utilities.Hologram;
-import org.bukkit.Location;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerExpChangeEvent;
-
-import java.util.concurrent.ThreadLocalRandom;
 
 public class LevelingEvent implements Listener {
 
@@ -23,27 +16,13 @@ public class LevelingEvent implements Listener {
     }
 
     @EventHandler
-    private void playerReceivedExp(PlayerExpChangeEvent event) {
+    private void braveReceivedExp(PlayerExpChangeEvent event) {
         // TODO: Exp mechanism
     }
 
     @EventHandler
-    private void entityDamageByPlayer(EntityDamageByEntityEvent event) {
-        Entity damager = event.getDamager();
-        Entity victim = event.getEntity();
-        if (damager instanceof Player && !(victim instanceof Player)) {
-            int damage = (int) event.getDamage();
-            Location victimLocation = victim.getLocation();
-            displayDamageHologram(damage, victimLocation);
-        }
-    }
-
-    private void displayDamageHologram(int damage, Location location) {
-        double x = ThreadLocalRandom.current().nextDouble(-1.25, 1.25);
-        double y = ThreadLocalRandom.current().nextDouble(0, 1.5);
-        double z = ThreadLocalRandom.current().nextDouble(-1.25, 1.25);
-        Hologram.drawTemporary("&c-" + damage + " &4" + Icons.RED_HEART, 10,
-                location.toCenterLocation().add(x, y, z));
+    private void mobDamageByBrave(EntityDamageByEntityEvent event) {
+        // TODO: Exp mechanism
     }
 
     @EventHandler
