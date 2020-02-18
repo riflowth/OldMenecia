e4b3ffd3c1e1ff327a5ffc797226ee1b41438632
RiFlowTH <riflowth@gmail.com>
2020-02-18 16:45:51 +0700

    Added base of PlayerSkillEvent

---
+++ src/main/java/net/projectx/menecia/player/events/PlayerSkillEvent.java
@@ -0,0 +1,13 @@
+package net.projectx.menecia.player.events;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class PlayerSkillEvent implements Listener {
+
+    @EventHandler
+    private void onRightClick() {
+
+    }
+
+}
