1588bbb482329bb24a5afee39e52238cc84e0684
RiFlowTH <riflowth@gmail.com>
2020-02-18 16:45:37 +0700

    Added AttackRange enum

---
+++ src/main/java/net/projectx/menecia/weapons/AttackRange.java
@@ -0,0 +1,13 @@
+package net.projectx.menecia.weapons;
+
+public enum AttackRange {
+
+    VERY_SHORT(1),
+    SHORT(2),
+    MEDIUM(3),
+    LONG(4),
+    VERY_LONG(5);
+
+    AttackRange(int blockRange) { }
+
+}
