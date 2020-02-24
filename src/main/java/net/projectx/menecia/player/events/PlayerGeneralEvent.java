62deebb97048947cb4002c2641558a881c3f150a
RiFlowTH <riflowth@gmail.com>
2020-02-24 23:37:38 +0700

    Added Player join effect

--- src/main/java/net/projectx/menecia/player/events/PlayerGeneralEvent.java
+++ src/main/java/net/projectx/menecia/player/events/PlayerGeneralEvent.java
@@ -11,11 +11,14 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class PlayerGeneralEvent implements Listener {
 
     private Menecia plugin;
     private PlayerManager playerManager;
+    private static final int joiningEffectTime = 3;
 
     public PlayerGeneralEvent(Menecia plugin) {
         this.plugin = plugin;
@@ -24,13 +27,24 @@ public class PlayerGeneralEvent implements Listener {
 
     @EventHandler
     private void onPlayerJoin(PlayerJoinEvent event) {
+        event.setJoinMessage(null);
         Player player = event.getPlayer();
         playerManager.add(player);
         TabListManager.initialize(player);
+        player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 20 * joiningEffectTime,
+                1, false, false, false));
+        player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20 * joiningEffectTime,
+                1000000, false, false, false));
+        player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 20 * joiningEffectTime,
+                -1000000, false, false, false));
+        plugin.runTaskLater(() -> {
+            player.sendTitle("", Utils.color("&7Are you alright?"), 10, 20 * 2, 20);
+        }, 20 * joiningEffectTime);
     }
 
     @EventHandler
     private void onPlayerQuit(PlayerQuitEvent event) {
+        event.setQuitMessage(null);
         playerManager.remove(event.getPlayer());
     }
 
